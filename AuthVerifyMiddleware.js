350d00cff5410bc02d48c3fad059cae4e8c898a5
Rahed Hossen <133135704+rahedhossen49@users.noreply.github.com>
2023-10-15 23:10:50 +0600

    Add files via upload

---
+++ AuthVerifyMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports= async(req, res, next)=>{
+    try {
+        let token = req.headers["token-key"];
+        const decoded = await jwt.verify(token, "Rahed49");
+
+        let email = decoded["data"];
+        req.headers.email = email;
+
+        next()
+    } catch (error) {
+        res.status(404).json({status: "fail",data: error});
+    }
+};
